5d8ae65ae3df727abd6b48cd9fcd9537bc3c316c
srish-web <mssrishtygupta1110@gmail.com>
2026-04-11 09:33:27 +0530

    activity1

--- my-first-react-app/src/Price.jsx
+++ my-first-react-app/src/Price.jsx
@@ -3,7 +3,7 @@ import "./Price.css";
 export default function Price({oldPrice, newPrice}){
     return(
         <>
-            <div>
+            <div className="priceBox">
                 <span className="old">{oldPrice}</span>
                 &nbsp;&nbsp;&nbsp;&nbsp;
                 <span className="new">{newPrice}</span>
